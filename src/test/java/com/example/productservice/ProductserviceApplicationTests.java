f5baa37908a56cca3a28835bb6cc892772fc3281
K-V-Sagar <sgr.venkat24@gmail.com>
2022-10-04 17:16:28 +0530

    update ==> 4/10/22

--- src/test/java/com/example/productservice/ProductserviceApplicationTests.java
+++ src/test/java/com/example/productservice/ProductserviceApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.productservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ProductserviceApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
